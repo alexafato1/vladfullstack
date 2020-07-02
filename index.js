ac2145d5f7a4a7d758bd800314b60d31bd784f56
Alexa fato <alexafato@MacBook-Air-Alexa.local>
2020-07-03 02:10:08 +0300

    creae

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const  PORT = process.env.PORT || 80
+
+
+app.get('/', (req, res) => {
+    res.end('<h1>Home</h1>')
+})
+
+
+app.get('/about', (req, res) => {
+    res.end('<h1>ABOUT</h1>')
+})
+
+
+app.listen(PORT, () => {
+    console.log('ПОЕХАЛИ!!!')
+})
