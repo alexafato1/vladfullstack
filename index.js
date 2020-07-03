89ce6c0faf42d8c5003676af392291cce1c91b16
Alexa fato <alexafato@MacBook-Air-Alexa.local>
2020-07-03 20:08:53 +0300

    vlad connect mongo

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+const path = require ('path')
+
 const app = express()
-const  PORT = process.env.PORT || 80
+const  PORT = process.env.PORT || 3000
 
-
-app.get('/', (req, res) => {
-    res.end('<h1>Home</h1>')
+const hbs = exphbs.create({
+   defaultLayout: 'main',
+   extname: 'hbs' 
 })
 
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-app.get('/about', (req, res) => {
-    res.end('<h1>ABOUT</h1>')
-})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
+
+app.use(todoRoutes)
 
 app.listen(PORT, () => {
     console.log('ПОЕХАЛИ!!!')
 })
+
+async function start() {
+   try{
+       await mongoose.connect('mongodb+srv://alex:happyday@cluster0.qcu7d.mongodb.net/merryalexa', {
+           useNewUrlParser: true,
+           useUnifiedTopology: true ,
+           useFindAndModify: false
+
+       })
+
+   }
+   catch (e){
+     console.log(e)
+   }
+}
+
+
+start()
+
